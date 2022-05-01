69406692e97e339c971503f19ff7ae0b91d0b943
zhangyue44 <zy1617768774@163.com>
2022-05-01 20:58:08 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "quill/dist/quill.snow.css";
+import "highlight.js/styles/monokai-sublime.css";
+// import "quill/dist/quill.bubble.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
